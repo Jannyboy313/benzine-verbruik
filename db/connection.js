76d302645481999ff0580994b3172aa87864d5b8
Jan <s1120303@student.hsleiden.nl>
2021-03-16 19:18:50 +0100

    Renamed files

--- db/connection.js
+++
@@ -1,9 +0,0 @@
-require('dotenv/config');
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("We are connected")
-});
