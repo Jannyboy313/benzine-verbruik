1a9ca8e46b005038aaa9aecdb8967a9593dc0627
Jan <s1120303@student.hsleiden.nl>
2021-03-16 19:31:54 +0100

    Created uri's

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.router();
+const userController = require('../controllers/userController.js');
 
-router.get('/user')
+router.post('/user', userController.postUser)
+router.get('/user/{id}', userController.getUser)
+router.put('/user/{id}', userController.putUser)
+router.delete('/user/{id}', userController.putUser)
