266b994721814b4334cf8bd20e95aa6fb969aeb2
Jan <s1120303@student.hsleiden.nl>
2021-03-16 17:57:14 +0100

    Removed body parser

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 require('dotenv/config');
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.json())
 
 //ROUTES
 app.use(cors());
