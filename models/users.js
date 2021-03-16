b81b517918ca1a9a15e876fed9896fc90ec00e27
Jan <s1120303@student.hsleiden.nl>
2021-03-16 17:41:05 +0100

    Added schema users

---
+++ models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UsersSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: {currentTime: () => {
+            let d = new Date();
+            return d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+}}}
+)
+
+module.exports = mongoose.model('Users', UsersSchema);
