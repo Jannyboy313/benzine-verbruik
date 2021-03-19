b5fb7c1d998a23463f171c54db22b48d9e4ede43
Jan <s1120303@student.hsleiden.nl>
2021-03-19 15:46:23 +0100

    Added print to know which port its running

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(cors());
 app.use('/user', userRouter);
 
 //LISTENER
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result) => app.listen(process.env.PORT))
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+    .then((result) => {
+        app.listen(process.env.PORT);
+        console.log(`Running on port ${process.env.PORT}`);
+    })
     .catch((err) => console.log(err));
