39b3b94e3f6f079e56d05fdc115be5f6b40ac6d1
Jan <s1120303@student.hsleiden.nl>
2021-03-19 09:46:09 +0100

    Added catch

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,15 @@
 const User = require('../models/user.js')
 
 exports.postUser = (req, res) => {
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
+    const user = new User(req.body);
     user.save()
         .then(result => {
             console.log("This works");
             console.log(result);
-        });
+        })
+        .catch(err => {
+            console.log("This doesn't work")
+            console.log(err)
+        })
     res.end();
 }
