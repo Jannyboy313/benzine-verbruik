62a3740d0ca9416365e54a049f246b18c664bd39
Jan <s1120303@student.hsleiden.nl>
2021-03-19 15:41:50 +0100

    Created functions for routes ride

--- controllers/rideController.js
+++ controllers/rideController.js
@@ -3,11 +3,64 @@ const Ride = require('../models/ride.js')
 exports.postRide = (req, res) => {
     const ride = new Ride(req.body);
     ride.save()
-        .then(result => {
-            res.status(201).send(result);
-        })
-        .catch(err => {
-            const status = err.statusCode || 500;
-            res.status(status).json({message: err})
-        });
+    .then(result => {
+        res.status(201).send(result);
+    })
+    .catch(err => {
+        const status = err.statusCode || 500;
+        res.status(status).json({message: err})
+    });
+}
+
+exports.getRides = (req, res) => {
+    Ride.find()
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        const status = err.statusCode || 500;
+        res.status(status).json({message: err})
+    });
+}
+
+exports.getRide = (req, res) => {
+    const id = req.params.id;
+    Ride.findById(id)
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        const status = err.statusCode || 500;
+        res.status(status).json({message: err})
+    });
+}
+
+exports.putRide = (req, res) => {
+    const id = req.params.id;
+    const ride = new Ride(req.body);
+    Ride.updateOne({ _id: id }, {
+        title: ride.title,
+        description: ride.description,
+        distance: ride.distance,
+        user: ride.user
+     })
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        const status = err.statusCode || 500;
+        res.status(status).json({message: err})
+    });
+}
+
+exports.deleteRide = (req, res) => {
+    const id = req.params.id;
+    Ride.deleteOne(id)
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        const status = err.statusCode || 500;
+        res.status(status).json({message: err})
+    });
 }
--- routes/rideRouter.js
+++ routes/rideRouter.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const rideController = require('../controllers/rideController.js');
 
 router.post('/', rideController.postRide);
-// router.get('/{id}', rideController.getRide);
-// router.get('/', rideController.getRides);
-// router.put('/{id}', rideController.putRide);
-// router.delete('/{id}', rideController.putRide);
+router.get('/', rideController.getRides);
+router.get('/{id}', rideController.getRide);
+router.put('/{id}', rideController.putRide);
+router.delete('/{id}', rideController.deleteRide);
 
 module.exports = router;
