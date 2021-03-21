bcf9c075aee74f4facaf81cd09b699bf4e824efb
Jan <s1120303@student.hsleiden.nl>
2021-03-21 18:50:09 +0100

    Changed config

--- config/passport.js
+++ config/passport.js
@@ -1,28 +1,23 @@
-import passport from 'passport'
-import passportJWT from 'passport-jwt'
-const JWTStrategy = passportJWT.Strategy
+require('dotenv/config')
+const JwtStrategy = require('passport-jwt/Strategy');
+const ExtractJwt = require('passport-jwt/ExtractJwt');
 
-const secret = process.env.JWT_SECRET
+const User = require('../models/user.js');
 
-const cookieExtractor = req => {
-    let jwt = null
-
-    if (req && req.cookies) {
-        jwt = req.cookies['jwt']
-    }
-
-    return jwt
+module.exports = function(passport) {
+    passport.use(new JwtStrategy({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+        secretOrKey: process.env.ACCESS_TOKEN
+    }, function (jwt_payload, done) {
+        User.findById(jwt_payload.data._id, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    }));
 }
-
-passport.use('jwt', new JWTStrategy({
-    jwtFromRequest: cookieExtractor,
-    secretOrKey: secret
-}, (jwtPayload, done) => {
-    const { expiration } = jwtPayload
-
-    if (Date.now() > expiration) {
-        done('Unauthorized', false)
-    }
-
-    done(null, jwtPayload)
-}))
