3f0c002cedb7646734f4a7861e387f5328c7f09b
Jan <s1120303@student.hsleiden.nl>
2021-03-21 22:04:08 +0100

    Fixed imports

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 require('dotenv/config')
-const JwtStrategy = require('passport-jwt/Strategy');
-const ExtractJwt = require('passport-jwt/ExtractJwt');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const User = require('../models/user.js');
 
