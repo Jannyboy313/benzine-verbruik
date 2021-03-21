77aa923a21ed3c695fa6bb52a5b2e91812d35cb1
Jan <s1120303@student.hsleiden.nl>
2021-03-21 22:01:40 +0100

    Added error handling

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -9,8 +9,13 @@ module.exports = async (req, res, next) => {
     jwt.verify(req.cookies['access-token'], process.env.ACCESS_TOKEN_SECRET, (err, authorizedData) => {
         if(err) {
             const newTokens = await auth.refreshTokens(req.cookies['refresh-token']);
-            res.cookie('access-token', newTokens.accesstoken, { maxAge: 3 * 60 * 60 * 1000 , httpOnly: true});
-            res.cookie('refresh-token', newTokens.refreshToken, { maxAge: 60 * 60 * 24 * 40 * 1000 , httpOnly: true});
+            if (!newTokens.error) {
+                res.cookie('access-token', newTokens.accesstoken, { maxAge: 3 * 60 * 60 * 1000 , httpOnly: true});
+                res.cookie('refresh-token', newTokens.refreshToken, { maxAge: 60 * 60 * 24 * 40 * 1000 , httpOnly: true});
+                next();
+            } else {
+                return res.status(401).json({ message: newTokens.message })
+            }
         } else {
             res.locals.user = authorizedData.user
             next();
