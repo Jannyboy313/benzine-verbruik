314c09725b12b4457b1e69c1c10a7d82a7a1dc18
Jan <s1120303@student.hsleiden.nl>
2021-03-21 22:13:58 +0100

    Cleaned up routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController.js');
 
 router.post('/login', userController.login)
-router.post('/', userController.postUser);
-// router.get('/', userController.getUsers);
+router.post('/register', userController.register);
 // router.get('/:id', userController.getUser);
 // router.put('/:id', userController.putUser);
-// router.delete('/:id', userController.putUser);
+// router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
