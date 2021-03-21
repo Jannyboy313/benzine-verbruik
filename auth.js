8ddbee3dde63f1465a46c0a1446cabbce873d274
Jan <s1120303@student.hsleiden.nl>
2021-03-21 21:37:14 +0100

    Renamed refresh function

--- auth.js
+++ auth.js
@@ -3,13 +3,13 @@ const User = require('./models/user.js');
 require('dotenv').config()
 
 module.exports.createTokens = (user) => {
-    const createAccessToken = jwt.sign({ user: user }, process.env.ACCESS_TOKEN, { expiresIn: "15m" });
-    const createRefreshToken = jwt.sign({ user: user }, process.env.REFRESH_TOKEN, { expiresIn: "14d" });
+    const createdAccessToken = jwt.sign({ user: user }, process.env.ACCESS_TOKEN, { expiresIn: "15m" });
+    const createdRefreshToken = jwt.sign({ user: user }, process.env.REFRESH_TOKEN, { expiresIn: "14d" });
 
-    return Promise.all([createAccessToken, createRefreshToken]);
+    return Promise.all([createdAccessToken, createdRefreshToken]);
 }
 
-module.exports.refreshTokens = async (refreshToken) => {
+module.exports.refreshAccessToken = async (refreshToken) => {
     let user;
     try {
         user = jwt.decode(refreshToken);
@@ -25,6 +25,6 @@ module.exports.refreshTokens = async (refreshToken) => {
         return err;
     }
 
-    const [newAccessToken, newRefreshToken] = await this.createTokens(updatedUser);
-    return { token: newAccessToken, refreshToken: newRefreshToken, user: user };
+    const createdAccessToken = jwt.sign({ user: updatedUser }, process.env.ACCESS_TOKEN, { expiresIn: "15m" });
+    return createdAccessToken;
 }
