f684df5161c1bf62237808a5a7845dbd75ba52ee
Jan <s1120303@student.hsleiden.nl>
2021-03-22 10:17:01 +0100

    Added swagger settings

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 require('./config/passport.js')(passport);
 const userRouter = require('./routes/userRouter.js');
@@ -16,6 +18,24 @@ const app = express();
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser());
 app.use(express.json());
+
+// Documentation initializing
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: 'Benzine API',
+        version: '1.0.0',
+        description: 'Api for the requests of the benzine app',
+      },
+      contact: {
+        name: "Jan van Overbeek",
+        email: "janvanoverbeek12@gmail.com"
+      },
+    },
+    apis: ['endpoints.js'],
+};
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsdoc(swaggerOptions)));
 
 // passport middleware
 app.use(passport.initialize());
