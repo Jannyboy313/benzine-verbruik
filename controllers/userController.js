1dbdc60b0eef16189cd372dc0e37cfb9a17f91dc
Jan <s1120303@student.hsleiden.nl>
2021-03-22 16:18:30 +0100

    Changed bcrypt package > bcryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.js');
 const auth = require('../auth.js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.login = async (req, res) => {
     const email = req.body.email;
