59531745ded7e77a5c29fde25086b78677b5257e
Jan <s1120303@student.hsleiden.nl>
2021-07-02 15:29:33 +0200

    Added router

--- PWA/src/app/login/login.module.ts
+++ PWA/src/app/login/login.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginRoutingModule } from './login-routing.module';
+
 import { LoginScreenComponent } from './login-screen/login-screen.component';
 
 
 
 @NgModule({
-  declarations: [
-    LoginScreenComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule
   ]
 })
 export class LoginModule { }
