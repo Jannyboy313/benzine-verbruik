0fee420fb1e13456da5870f030297e13c2937042
Jan <s1120303@student.hsleiden.nl>
2021-07-08 00:12:31 +0200

    Added error model

--- PWA/src/app/login/login-screen/login-screen.component.ts
+++ PWA/src/app/login/login-screen/login-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Error } from '../../../shared/models/error.model';
 
 @Component({
   selector: 'app-login-screen',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginScreenComponent implements OnInit {
 
-  error: any = {
+  error: Error = {
     isError: false,
     message: ''
   }
---
+++ PWA/src/shared/models/error.model.ts
@@ -0,0 +1,9 @@
+export class Error {
+    isError: boolean;
+    message: string;
+
+    constructor(isError: boolean, message: string, createdAt: Date, updatedAt: Date) {
+        this.isError = isError;
+        this.message = message;
+    }
+}
