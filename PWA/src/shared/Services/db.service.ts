9f1ba616640da842803a95dc1e9d89e663b54c8d
Jan <s1120303@student.hsleiden.nl>
2021-07-07 23:35:00 +0200

    Created db service

---
+++ PWA/src/shared/Services/db.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class Db {
+    dbAddress = environment.api_url;
+
+    constructor(private http: HttpClient) {}
+
+    sendGetRequest(urlPath: string): Observable<any> {
+      return this.http.get<any>(this.dbAddress + urlPath, this.generateOptions());
+    }
+
+    sendPostRequest(urlPath: string, body: unknown): Observable<any> {
+      return this.http.post<any>(this.dbAddress + urlPath, body, this.generateOptions());
+    }
+
+    sendPutRequest(urlPath: string, body: unknown): Observable<any> {
+      return this.http.put<any>(this.dbAddress + urlPath, body, this.generateOptions());
+    }
+
+    sendDeleteRequest(urlPath: string): Observable<any> {
+      return this.http.delete<any>(this.dbAddress + urlPath, this.generateOptions());
+    }
+
+    private generateOptions(): { headers: HttpHeaders, withCredentials: boolean } {
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+      });
+      return { headers, withCredentials: true };
+    }
+}
