8a96897ecee1753ba42c892a758a1ebcd18293ff
Jan <s1120303@student.hsleiden.nl>
2021-07-08 12:01:17 +0200

    Added loginservice for better OO principles

--- PWA/src/shared/Services/data-storage.service.ts
+++ PWA/src/shared/Services/data-storage.service.ts
@@ -20,15 +20,4 @@ export class DataStorageService {
     localStorage.clear();
   }
 
-  isLoggedIn(): boolean {
-    const cookies = document.cookie.split(';');
-    for (let i=0; i < cookies.length; i++) {
-      let cookie = cookies[i];
-      while (cookie.charAt(0) == ' ') cookie = cookie.substring(1, cookie.length);
-      if (cookie.indexOf("refresh-token=") == 0)
-        return true;
-    }
-    return false;
-  }
-
 }
