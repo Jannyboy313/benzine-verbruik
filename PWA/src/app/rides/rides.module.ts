e478cd84023d121d5bf4246c1c1cce357849fd5b
Jan <s1120303@student.hsleiden.nl>
2021-07-09 14:42:17 +0200

    Added spinner

--- PWA/src/app/rides/rides.module.ts
+++ PWA/src/app/rides/rides.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RidesRoutingModule } from './rides-routing.module';
+
 import { RidesScreenComponent } from './rides-screen/rides-screen.component';
 import { RidesListComponent } from './rides-list/rides-list.component';
 import { RideCardComponent } from './rides-list/ride-card/ride-card.component';
+import { SpinnerComponent } from 'src/shared/components/spinner/spinner.component';
 
 
 @NgModule({
   declarations: [
     RidesScreenComponent,
     RidesListComponent,
-    RideCardComponent
+    RideCardComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule,
