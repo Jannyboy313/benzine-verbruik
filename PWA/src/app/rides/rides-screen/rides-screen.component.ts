c91c1dd7507a8753df8ff430b711f88cb51aec3c
Jan <s1120303@student.hsleiden.nl>
2021-07-09 10:36:53 +0200

    Added ridesScreen component

---
+++ PWA/src/app/rides/rides-screen/rides-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rides-screen',
+  templateUrl: './rides-screen.component.html',
+  styleUrls: ['./rides-screen.component.scss']
+})
+export class RidesScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- PWA/src/app/rides/rides.module.ts
+++ PWA/src/app/rides/rides.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RidesRoutingModule } from './rides-routing.module';
+import { RidesScreenComponent } from './rides-screen/rides-screen.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RidesScreenComponent
+  ],
   imports: [
     CommonModule,
     RidesRoutingModule
