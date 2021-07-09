6d9c66c163476d987bbdea52cf92e8cf160501ef
Jan <s1120303@student.hsleiden.nl>
2021-07-09 10:32:23 +0200

    Generated rides module

---
+++ PWA/src/app/rides/rides.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RidesRoutingModule } from './rides-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RidesRoutingModule
+  ]
+})
+export class RidesModule { }
