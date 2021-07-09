090322a14ce167a92325ce794172de3ebd834f1a
Jan <s1120303@student.hsleiden.nl>
2021-07-09 14:55:48 +0200

    Added spinner to shared module to use everywhere

--- PWA/src/app/login/login.module.ts
+++ PWA/src/app/login/login.module.ts
@@ -5,10 +5,10 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
 import { FormInputComponent } from './form-input/form-input.component';
 import { ErrorSpanComponent } from 'src/shared/components/error-span/error-span.component';
-import { SpinnerComponent } from 'src/shared/components/spinner/spinner.component';
 
 import { UserService } from 'src/shared/Services/db/user.service';
 import { DataStorageService } from 'src/shared/Services/data-storage.service';
+import { SharedModule } from 'src/shared/shared.module';
 
 
 
@@ -16,17 +16,16 @@ import { DataStorageService } from 'src/shared/Services/data-storage.service';
   declarations: [
     LoginScreenComponent,
     FormInputComponent,
-    ErrorSpanComponent,
-    SpinnerComponent
+    ErrorSpanComponent
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    SharedModule
   ],
   providers: [
     UserService,
     DataStorageService
-
   ]
 })
 export class LoginModule { }
--- PWA/src/app/rides/rides.module.ts
+++ PWA/src/app/rides/rides.module.ts
@@ -6,19 +6,23 @@ import { RidesRoutingModule } from './rides-routing.module';
 import { RidesScreenComponent } from './rides-screen/rides-screen.component';
 import { RidesListComponent } from './rides-list/rides-list.component';
 import { RideCardComponent } from './rides-list/ride-card/ride-card.component';
-import { SpinnerComponent } from 'src/shared/components/spinner/spinner.component';
+import { RideService } from 'src/shared/Services/db/ride.service';
+import { SharedModule } from 'src/shared/shared.module';
 
 
 @NgModule({
   declarations: [
     RidesScreenComponent,
     RidesListComponent,
-    RideCardComponent,
-    SpinnerComponent
+    RideCardComponent
   ],
   imports: [
     CommonModule,
-    RidesRoutingModule
+    RidesRoutingModule,
+    SharedModule
+  ],
+  providers: [
+    RideService
   ]
 })
 export class RidesModule { }
