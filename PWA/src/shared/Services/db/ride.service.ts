ebe81dfb3da1c64a86d6290ed1071b200fdcc8b6
Jan <s1120303@student.hsleiden.nl>
2021-07-09 14:25:41 +0200

    Created ride service http requests

---
+++ PWA/src/shared/Services/db/ride.service.ts
@@ -0,0 +1,25 @@
+import { Db } from '../../../util/db';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+import { Ride } from '../../models/ride.model';
+
+@Injectable()
+export class UserService {
+
+  constructor(private db: Db) {}
+
+    getRides(id: string): Observable<Ride[]> {
+    return this.db.sendGetRequest(`ride`)
+    .pipe(map((response: Ride) => {
+      return response;
+    }));
+  }
+
+  getRide(id: string): Observable<Ride> {
+    return this.db.sendGetRequest(`ride/${id}`)
+    .pipe(map((response: Ride) => {
+      return response;
+    }));
+  }
+}
