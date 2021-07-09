886034dd0b366de6c54c847317f88714e2ab25bf
Jan <s1120303@student.hsleiden.nl>
2021-07-09 10:36:32 +0200

    Added rides route

--- PWA/src/app/rides/rides-routing.module.ts
+++ PWA/src/app/rides/rides-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RidesScreenComponent } from './rides-screen/rides-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RidesScreenComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
