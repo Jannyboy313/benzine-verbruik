3c49d348fbe49ae1969fb921a437662eb96eeab4
Jan <s1120303@student.hsleiden.nl>
2021-07-10 19:28:23 +0200

    Added date formatter

--- PWA/src/app/rides/rides-list/ride-card/ride-card.component.ts
+++ PWA/src/app/rides/rides-list/ride-card/ride-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Ride } from 'src/shared/models/ride.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-ride-card',
@@ -17,6 +18,12 @@ export class RideCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  formatDate(date: Date | undefined) {
+    moment.locale('nl');
+    let formattedDate = moment(date).format('ddd DD MMM YYYY HH:mm'); // Woe 17 Mrt 2021 14:31
+    return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1).replace(/\./g, "");
   }
 
 }
