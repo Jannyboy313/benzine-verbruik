d77a7d8b5410f3eee089ad31df2c650fc7fa8b5c
Jan <s1120303@student.hsleiden.nl>
2021-07-11 21:51:57 +0200

    Fixed navigation not showing at login

--- PWA/src/app/app.component.ts
+++ PWA/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'benzine-pwa';
+
+	constructor(private router: Router) {}
+
+	isLogin() {
+		const routerPath = this.router.url.split('/')[1];
+		if (routerPath === 'home') {
+			return true;
+		}
+		return false;
+	}
 }
