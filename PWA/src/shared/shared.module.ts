bdd1361d68574948c1ac777b27c69894de04d6b0
Jan <s1120303@student.hsleiden.nl>
2021-07-11 18:13:55 +0200

    Generated new bottom navigation component

--- PWA/src/shared/shared.module.ts
+++ PWA/src/shared/shared.module.ts
@@ -4,11 +4,13 @@ import { MatDialogModule } from '@angular/material/dialog';
 
 import { ErrorSpanComponent } from './components/error-span/error-span.component';
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { BottomNavigationComponent } from './components/bottom-navigation/bottom-navigation.component';
 
 @NgModule({
   declarations: [
     ErrorSpanComponent,
-    ConfirmDialogComponent
+    ConfirmDialogComponent,
+    BottomNavigationComponent
   ],
   imports: [
     CommonModule,
