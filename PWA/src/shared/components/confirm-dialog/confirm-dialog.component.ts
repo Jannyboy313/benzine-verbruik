0ceb5cb9bfe836c884dedd20722fe1918149a2cf
Jan <s1120303@student.hsleiden.nl>
2021-07-11 17:45:03 +0200

    Added styling and logic dialog

--- PWA/src/shared/components/confirm-dialog/confirm-dialog.component.ts
+++ PWA/src/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.scss']
 })
-export class ConfirmDialogComponent implements OnInit {
+export class ConfirmDialogComponent {
 
-  constructor() { }
+constructor(
+  public dialogRef: MatDialogRef<any>,
+  @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
-  ngOnInit(): void {
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
