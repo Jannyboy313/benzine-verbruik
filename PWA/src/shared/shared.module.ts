ebeb0b648b4ce7a85424eceb041e6196bec82631
Jan <s1120303@student.hsleiden.nl>
2021-07-11 18:14:37 +0200

    Imported bottom navigation module

--- PWA/src/shared/shared.module.ts
+++ PWA/src/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
+import { BottomNavModule } from 'ngx-bottom-nav';
 
 import { ErrorSpanComponent } from './components/error-span/error-span.component';
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
@@ -14,7 +15,8 @@ import { BottomNavigationComponent } from './components/bottom-navigation/bottom
   ],
   imports: [
     CommonModule,
-    MatDialogModule
+    MatDialogModule,
+    BottomNavModule
   ],
   exports: [
     ErrorSpanComponent,
