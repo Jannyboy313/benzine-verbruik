febacf0ac64d730921835334ebb66bb31f08a002
Jan <s1120303@student.hsleiden.nl>
2021-07-11 14:24:01 +0200

    Generated confirm-dialog component

--- PWA/src/shared/shared.module.ts
+++ PWA/src/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ErrorSpanComponent } from './components/error-span/error-span.component';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
-    ErrorSpanComponent
+    ErrorSpanComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ErrorSpanComponent
+    ErrorSpanComponent,
+    ConfirmDialogComponent
   ],
 })
 export class SharedModule { }
