9b4e64881b64b072528904b8b734983b2474963e
Jan <s1120303@student.hsleiden.nl>
2021-07-12 11:53:10 +0200

    Added form module

--- PWA/src/app/rides/rides.module.ts
+++ PWA/src/app/rides/rides.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { RidesRoutingModule } from './rides-routing.module';
 import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { RidesScreenComponent } from './rides-screen/rides-screen.component';
 import { RidesListComponent } from './rides-list/rides-list.component';
@@ -12,8 +13,20 @@ import { SharedModule } from 'src/shared/shared.module';
 import { RideModalComponent } from './ride-modal/ride-modal.component';
 
 @NgModule({
-	declarations: [RidesScreenComponent, RidesListComponent, RideCardComponent, RideModalComponent],
-	imports: [CommonModule, RidesRoutingModule, SharedModule, MatDialogModule],
+	declarations: [
+		RidesScreenComponent,
+		RidesListComponent,
+		RideCardComponent,
+		RideModalComponent
+	],
+	imports: [
+		CommonModule,
+		RidesRoutingModule,
+		SharedModule,
+		MatDialogModule,
+		FormsModule,
+		ReactiveFormsModule
+	],
 	providers: [RideService]
 })
 export class RidesModule {}
