6cf7b7a2f3c08081697ed40b11d2386792402851
Jan <s1120303@student.hsleiden.nl>
2021-07-12 12:12:34 +0200

    Added create dialog

--- PWA/src/app/rides/rides-screen/rides-screen.component.ts
+++ PWA/src/app/rides/rides-screen/rides-screen.component.ts
@@ -1,4 +1,6 @@
+import { RideModalComponent } from './../ride-modal/ride-modal.component';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
 	selector: 'app-rides-screen',
@@ -6,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./rides-screen.component.scss']
 })
 export class RidesScreenComponent implements OnInit {
-	constructor() {}
+	constructor(public dialog: MatDialog) {}
 
 	ngOnInit(): void {}
+
+	openCreateModal(): void {
+		let dialogRef = this.dialog.open(RideModalComponent, {
+			data: {
+				header: `Aanmaken nieuwe rit`
+			}
+		});
+
+		dialogRef.afterClosed().subscribe(result => {
+			console.log("Im the result!")
+			// if (result) {
+			// 	this.rideService.deleteRide(this.ride._id);
+			// }
+		});
+	}
 }
