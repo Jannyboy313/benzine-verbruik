3dc07dad0bd057a469a873c2048caeecce1a3834
Jan <s1120303@student.hsleiden.nl>
2021-07-12 21:35:38 +0200

    Changed back to localhost

--- PWA/src/environments/environment.ts
+++ PWA/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
 	production: false,
-	// api_url: `http://localhost:3000/`
-	api_url: `https://benzine-api.janvanoverbeek.nl/`
+	api_url: `http://localhost:3000/`
+	// api_url: `https://benzine-api.janvanoverbeek.nl/`
 };
 
 /*
