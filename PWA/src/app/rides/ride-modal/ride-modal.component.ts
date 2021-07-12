13140fa365052eeb185141dc107738f67e9d85f6
Jan <s1120303@student.hsleiden.nl>
2021-07-12 12:14:17 +0200

    Created functional modal

--- PWA/src/app/rides/ride-modal/ride-modal.component.ts
+++ PWA/src/app/rides/ride-modal/ride-modal.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-ride-modal',
-  templateUrl: './ride-modal.component.html',
-  styleUrls: ['./ride-modal.component.scss']
+	selector: 'app-ride-modal',
+	templateUrl: './ride-modal.component.html',
+	styleUrls: ['./ride-modal.component.scss']
 })
 export class RideModalComponent implements OnInit {
+	isError: boolean = false;
 
-  constructor() { }
+	form: FormGroup = new FormGroup({
+		title: new FormControl(''),
+		description: new FormControl(''),
+		distance: new FormControl()
+	});
 
-  ngOnInit(): void {
+	constructor(
+		public dialogRef: MatDialogRef<any>,
+		@Inject(MAT_DIALOG_DATA)
+		public data: any = {
+			header: 'Aanmaken nieuwe rit',
+			title: '',
+			description: ''
+		}
+	) {}
+
+	ngOnInit() {
+		this.setInputValues();
+	}
+
+	setInputValues() {
+		if (this.data.title !== '') {
+			this.form.controls['title'].setValue(this.data.title);
+			this.form.controls['description'].setValue(this.data.description);
+			this.form.controls['distance'].setValue(this.data.distance);
+		}
+	}
+
+	onNoClick(): void {
+		this.dialogRef.close();
+	}
+
+	getActionName(): string {
+		return this.data.header.split(' ')[0];
+	}
+
+  onSubmit(): void {
+    
   }
-
 }
