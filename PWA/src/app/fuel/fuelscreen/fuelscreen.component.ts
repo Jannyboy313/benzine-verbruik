2ac78b576e820d3438da30b114c431219a59a9ac
Jan <s1120303@student.hsleiden.nl>
2021-07-13 12:18:43 +0200

    Added fuelscreen html

--- PWA/src/app/fuel/fuelscreen/fuelscreen.component.ts
+++ PWA/src/app/fuel/fuelscreen/fuelscreen.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fuelscreen',
-  templateUrl: './fuelscreen.component.html',
-  styleUrls: ['./fuelscreen.component.scss']
+	selector: 'app-fuelscreen',
+	templateUrl: './fuelscreen.component.html',
+	styleUrls: ['./fuelscreen.component.scss']
 })
 export class FuelscreenComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
+	openCreateModal() {
+
   }
-
 }
