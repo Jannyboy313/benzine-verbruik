7cee98d1fffb95b6e5d1fd63038ad69fd1d56720
Jan <s1120303@student.hsleiden.nl>
2021-07-13 13:30:45 +0200

    Updated model with 2 new properties for better grahps

--- API/models/fuel.js
+++ API/models/fuel.js
@@ -2,24 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var Float = require('mongoose-float').loadType(mongoose);
 
-const FuelSchema = new Schema({
-    liter: {
-        type: Float,
-        required: [true, 'Amount of fuel in liters is required']
-    },
-    price: {
-        type: Float,
-        required: [true, 'Price is required']
-    },
-    location: {
-        type: String,
-        maxLength: [25, "Location can't be longer then 25 characters"],
-        trim: true
-    },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }
-}, { timestamps: true });
+const FuelSchema = new Schema(
+	{
+		liter: {
+			type: Float,
+			required: [true, 'Amount of fuel in liters is required']
+		},
+		price: {
+			type: Float,
+			required: [true, 'Price is required']
+		},
+		gas_station: {
+			type: String,
+			maxLength: [15, "Gas station can't be longer then 15 characters"],
+			trim: true
+		},
+		location: {
+			type: String,
+			maxLength: [20, "Location can't be longer then 20 characters"],
+			trim: true
+		},
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'User'
+		}
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('Fuel', FuelSchema);
