88e7824022a8bf02c1b1bdef32807861cf7e6ed2
Jan <s1120303@student.hsleiden.nl>
2021-07-13 12:39:11 +0200

    Added shared module

--- PWA/src/app/fuel/fuel.module.ts
+++ PWA/src/app/fuel/fuel.module.ts
@@ -4,10 +4,10 @@ import { FuelScreenComponent } from './fuel-screen/fuel-screen.component';
 import { FuelRoutingModule } from './fuel-routing.module';
 import { FuelListComponent } from './fuel-list/fuel-list.component';
 import { FuelCardComponent } from './fuel-list/fuel-card/fuel-card.component';
-
+import { SharedModule } from 'src/shared/shared.module';
 
 @NgModule({
 	declarations: [FuelScreenComponent, FuelListComponent, FuelCardComponent],
-	imports: [CommonModule, FuelRoutingModule]
+	imports: [CommonModule, FuelRoutingModule, SharedModule]
 })
 export class FuelModule {}
