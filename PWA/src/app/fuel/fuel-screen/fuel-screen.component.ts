9323a9b82a8e8f8c2e69a89170b0b9d6893bf6f0
Jan <s1120303@student.hsleiden.nl>
2021-07-13 16:25:06 +0200

    Added create modal functionality

--- PWA/src/app/fuel/fuel-screen/fuel-screen.component.ts
+++ PWA/src/app/fuel/fuel-screen/fuel-screen.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { FuelListService } from 'src/shared/Services/fuel-list-service';
+import { FuelModalComponent } from '../fuel-modal/fuel-modal.component';
 
 @Component({
 	selector: 'app-fuel-screen',
@@ -6,11 +9,27 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./fuel-screen.component.scss']
 })
 export class FuelScreenComponent implements OnInit {
-	constructor() {}
+	constructor(
+		public dialog: MatDialog,
+		private fuelListService: FuelListService
+	) {}
 
 	ngOnInit(): void {}
 
-	openCreateModal() {
 
+	openCreateModal(): void {
+		let dialogRef = this.dialog.open(FuelModalComponent, {
+			width: '85vw',
+			maxWidth: '85vw',
+			data: {
+				header: `Aanmaken nieuwe tankbeurt`
+			}
+		});
+
+		dialogRef.afterClosed().subscribe(result => {
+			if (result) {
+				this.fuelListService.addFuel(result);
+			}
+		});
   }
 }
