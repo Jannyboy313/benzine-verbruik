136d68fef3432635cb551f91b3aedf00fcc4ca9f
Jan <s1120303@student.hsleiden.nl>
2021-07-13 12:55:54 +0200

    Added formatDate function

--- PWA/src/app/fuel/fuel-list/fuel-card/fuel-card.component.ts
+++ PWA/src/app/fuel/fuel-list/fuel-card/fuel-card.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-fuel-card',
-  templateUrl: './fuel-card.component.html',
-  styleUrls: ['./fuel-card.component.scss']
+	selector: 'app-fuel-card',
+	templateUrl: './fuel-card.component.html',
+	styleUrls: ['./fuel-card.component.scss']
 })
 export class FuelCardComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	formatDate(date: Date | undefined) {
+		moment.locale('nl');
+		let formattedDate = moment(date).format('ddd DD MMM YYYY HH:mm'); // Woe 17 Mrt 2021 14:31
+		return (
+			formattedDate.charAt(0).toUpperCase() +
+			formattedDate.slice(1).replace(/\./g, '')
+		);
+	}
 }
