3f116e2021cee077efb9f507e70c43bbef13d719
Jan <s1120303@student.hsleiden.nl>
2021-07-13 12:24:35 +0200

    Added fuel list component

---
+++ PWA/src/app/fuel/fuel-list/fuel-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fuel-list',
+  templateUrl: './fuel-list.component.html',
+  styleUrls: ['./fuel-list.component.scss']
+})
+export class FuelListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- PWA/src/app/fuel/fuel.module.ts
+++ PWA/src/app/fuel/fuel.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FuelscreenComponent } from './fuelscreen/fuelscreen.component';
 import { FuelRoutingModule } from './fuel-routing.module';
+import { FuelListComponent } from './fuel-list/fuel-list.component';
 
 
 @NgModule({
   declarations: [
-    FuelscreenComponent
+    FuelscreenComponent,
+    FuelListComponent
   ],
   imports: [
     CommonModule,
