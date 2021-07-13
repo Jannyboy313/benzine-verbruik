8bfed0ad88eb69c8c478a587f66739567208f696
Jan <s1120303@student.hsleiden.nl>
2021-07-13 15:38:05 +0200

    Added http get for fuel

--- PWA/src/app/fuel/fuel-list/fuel-list.component.ts
+++ PWA/src/app/fuel/fuel-list/fuel-list.component.ts
@@ -1,6 +1,8 @@
+import { FuelListService } from './../../../shared/Services/fuel-list-service';
 import { Component, OnInit } from '@angular/core';
 import { Error } from 'src/shared/models/error.model';
 import { FuelService } from 'src/shared/Services/db/fuel.service';
+import { Fuel } from 'src/shared/models/fuel.model';
 
 @Component({
 	selector: 'app-fuel-list',
@@ -14,15 +16,51 @@ export class FuelListComponent implements OnInit {
 		message: 'Network error'
 	};
 
-	constructor(private fuelService: FuelService) {}
+	fuelList: Fuel[] = [];
 
-	ngOnInit(): void {}
+	constructor(
+		private fuelService: FuelService,
+		private fuelListService: FuelListService
+	) {}
+
+	ngOnInit(): void {
+		this.fuelListService.getFuelSubject().subscribe(result => {
+			this.fuelList = result;
+		});
+		this.getFuel();
+	}
+
+	getFuel() {
+		this.setError(
+			false,
+			'There has been an error while trying to load the fuel'
+		);
+		this.isLoading = true;
+		this.fuelService.getFuels().subscribe(
+			result => {
+				this.isLoading = false;
+				this.fuelListService.setFuel(result);
+			},
+			err => {
+				this.setError(true, err.error.message);
+				this.isLoading = false;
+			}
+		);
+	}
 
 	fuelExist(): boolean {
-		return true;
+		if (this.fuelList.length > 0) {
+			return true;
+		}
+		return false;
 	}
 
 	closeError(isError: boolean) {
 		this.error.isError = isError;
+	}
+
+	private setError(isError: boolean, message: string): void {
+		this.error.isError = isError;
+		this.error.message = message;
 	}
 }
