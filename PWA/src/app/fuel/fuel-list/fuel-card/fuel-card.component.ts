b396397768a4f4dfc7cb1165b97fab334218c616
Jan <s1120303@student.hsleiden.nl>
2021-07-13 12:25:17 +0200

    Added fuel card component

---
+++ PWA/src/app/fuel/fuel-list/fuel-card/fuel-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fuel-card',
+  templateUrl: './fuel-card.component.html',
+  styleUrls: ['./fuel-card.component.scss']
+})
+export class FuelCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- PWA/src/app/fuel/fuel.module.ts
+++ PWA/src/app/fuel/fuel.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FuelscreenComponent } from './fuelscreen/fuelscreen.component';
 import { FuelRoutingModule } from './fuel-routing.module';
 import { FuelListComponent } from './fuel-list/fuel-list.component';
+import { FuelCardComponent } from './fuel-list/fuel-card/fuel-card.component';
 
 
 @NgModule({
   declarations: [
     FuelscreenComponent,
-    FuelListComponent
+    FuelListComponent,
+    FuelCardComponent
   ],
   imports: [
     CommonModule,
