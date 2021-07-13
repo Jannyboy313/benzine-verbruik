a3030326bd047121af80e4e576a2f33a4a5c27cd
Jan <s1120303@student.hsleiden.nl>
2021-07-13 15:23:39 +0200

    Added fuel service with http requests

---
+++ PWA/src/shared/Services/db/fuel.service.ts
@@ -0,0 +1,42 @@
+import { Db } from '../../../util/db';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Fuel } from '../../models/fuel.model';
+
+@Injectable()
+export class FuelService {
+	constructor(private db: Db) {}
+
+	postFuel(fuel: Fuel): Observable<Fuel> {
+		return this.db.sendPostRequest(`fuel`, fuel).pipe(
+			map((response: Fuel) => {
+				return response;
+			})
+		);
+	}
+
+	getFuels(): Observable<Fuel[]> {
+		return this.db.sendGetRequest(`fuel`).pipe(
+			map((response: Fuel[]) => {
+				return response;
+			})
+		);
+	}
+
+	getFuel(id: string): Observable<Fuel> {
+		return this.db.sendGetRequest(`fuel/${id}`).pipe(
+			map((response: Fuel) => {
+				return response;
+			})
+		);
+	}
+
+	putFuel(fuel: Fuel): Observable<Fuel> {
+		return this.db.sendPutRequest(`fuel/${fuel._id}`, fuel);
+	}
+
+	deleteFuel(id: string | undefined): Observable<any> {
+		return this.db.sendDeleteRequest(`fuel/${id}`);
+	}
+}
