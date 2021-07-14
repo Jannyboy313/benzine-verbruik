0434a2d5ebe78141f19fa640f5db211f8cbf274f
Jan <s1120303@student.hsleiden.nl>
2021-07-14 16:47:05 +0200

    Added error

--- PWA/src/app/dashboard/dashboard-screen/dashboard-screen.component.ts
+++ PWA/src/app/dashboard/dashboard-screen/dashboard-screen.component.ts
@@ -1,5 +1,6 @@
 import { DashboardService } from './../../../shared/Services/db/dashboard.service';
 import { Component, OnInit } from '@angular/core';
+import { Error } from 'src/shared/models/error.model';
 
 @Component({
 	selector: 'app-dashboard-screen',
@@ -7,14 +8,18 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./dashboard-screen.component.scss']
 })
 export class DashboardScreenComponent implements OnInit {
-
-  isLoading: boolean = true;
+	isLoading: boolean = true;
 
 	dashboardData: any = {
 		litres: 0,
 		prices: 0,
 		distance: 0,
-    balance: 0
+		balance: 0
+	};
+
+	error: Error = {
+		isError: false,
+		message: 'Network error'
 	};
 
 	constructor(private dashboardService: DashboardService) {}
@@ -22,15 +27,26 @@ export class DashboardScreenComponent implements OnInit {
 	ngOnInit(): void {}
 
 	getDashboardData() {
-    this.isLoading = true;
+		this.setError(false, 'Network error');
+		this.isLoading = true;
 		this.dashboardService.getDashboardData().subscribe(
 			result => {
-        this.isLoading = false;
-        this.dashboardData = result;
-      },
+				this.isLoading = false;
+				this.dashboardData = result;
+			},
 			err => {
-        this.isLoading = false;
-      }
+				this.isLoading = false;
+				this.setError(true, err.error.message);
+			}
 		);
+	}
+
+	closeError(isError: boolean) {
+		this.error.isError = isError;
+	}
+
+	private setError(isError: boolean, message: string): void {
+		this.error.isError = isError;
+		this.error.message = message;
 	}
 }
