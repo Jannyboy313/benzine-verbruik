fe8f1ff3e20b82c95bead842883a9af22ba16808
Jan <s1120303@student.hsleiden.nl>
2021-07-14 15:35:48 +0200

    Added barchart component

--- PWA/src/app/dashboard/dashboard.module.ts
+++ PWA/src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardScreenComponent } from './dashboard-screen/dashboard-screen.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 
 @NgModule({
   declarations: [
-    DashboardScreenComponent
+    DashboardScreenComponent,
+    BarChartComponent
   ],
   imports: [
     CommonModule,
