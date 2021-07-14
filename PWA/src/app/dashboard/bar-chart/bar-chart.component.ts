13f352f89d11f1b7b4a4f4591e9a3441d90ef1be
Jan <s1120303@student.hsleiden.nl>
2021-07-14 16:17:23 +0200

    Added input variables

--- PWA/src/app/dashboard/bar-chart/bar-chart.component.ts
+++ PWA/src/app/dashboard/bar-chart/bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-bar-chart',
@@ -6,9 +6,23 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./bar-chart.component.scss']
 })
 export class BarChartComponent implements OnInit {
+
+  @Input() name: string = '';
+  @Input() value: any = {
+    amount: 0,
+    total: 0
+  };
+
 	percentage: number = 40;
 
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+    this.percentage = this.calcPercentage();
+  }
+
+  calcPercentage(): number {
+    let percentage = this.value.amount / this.value.total;
+    return Math.round(percentage);
+  }
 }
