78fab5d704fd047e4b0295f9a9ff1012ce10f6af
Jan <s1120303@student.hsleiden.nl>
2021-07-14 16:34:11 +0200

    Added dashboard service

--- PWA/src/app/dashboard/dashboard-screen/dashboard-screen.component.ts
+++ PWA/src/app/dashboard/dashboard-screen/dashboard-screen.component.ts
@@ -1,3 +1,4 @@
+import { DashboardService } from './../../../shared/Services/db/dashboard.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dashboardService: DashboardService) { }
 
   ngOnInit(): void {
   }
