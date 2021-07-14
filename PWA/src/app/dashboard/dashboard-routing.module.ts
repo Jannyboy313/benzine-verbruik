ae76f7a9bf462440403d0d5b0de2ef17e96387ff
Jan <s1120303@student.hsleiden.nl>
2021-07-14 15:33:46 +0200

    Added screen component

--- PWA/src/app/dashboard/dashboard-routing.module.ts
+++ PWA/src/app/dashboard/dashboard-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardScreenComponent } from './dashboard-screen/dashboard-screen.component';
 
 const routes: Routes = [
 	{
--- PWA/src/app/dashboard/dashboard.module.ts
+++ PWA/src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardScreenComponent } from './dashboard-screen/dashboard-screen.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardScreenComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
