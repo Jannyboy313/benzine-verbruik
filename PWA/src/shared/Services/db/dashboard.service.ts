b137edc0e58b995f90daecec384d1032883f3551
Jan <s1120303@student.hsleiden.nl>
2021-07-14 16:53:42 +0200

    Added pipe to request

--- PWA/src/shared/Services/db/dashboard.service.ts
+++ PWA/src/shared/Services/db/dashboard.service.ts
@@ -1,12 +1,18 @@
 import { Db } from '../../../util/db';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Dashboard } from 'src/shared/models/dashboard.model';
 
 @Injectable()
 export class DashboardService {
 	constructor(private db: Db) {}
 
-	getDashboardData(): Observable<any> {
-		return this.db.sendGetRequest(`dashboard`);
+	getDashboardData(): Observable<Dashboard> {
+		return this.db.sendGetRequest(`dashboard`).pipe(
+			map((response: Dashboard) => {
+				return response;
+			})
+		);
 	}
 }
