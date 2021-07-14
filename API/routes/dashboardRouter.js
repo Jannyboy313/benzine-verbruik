61cd9563f7e11ebe74407826c41f911685e0b307
Jan <s1120303@student.hsleiden.nl>
2021-07-14 13:07:01 +0200

    Added function to route

--- API/routes/dashboardRouter.js
+++ API/routes/dashboardRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const addUserId = require('../middleware/addUserId.js');
 const dashboardController = require('../controllers/dashboardController.js');
 
-router.get('/', dashboardController);
+router.get('/', dashboardController.getDashboardData);
 // router.get('/day/:id', addUserId, dashboardController);
 // router.get('/month/:id', addUserId, dashboardController);
 // router.get('/year/:id', addUserId, dashboardController);
