e6fbcfeba4688963355e022e42eb73f142d25ea8
Jan <s1120303@student.hsleiden.nl>
2021-07-14 16:38:12 +0200

    Added http request for data

--- PWA/src/app/dashboard/dashboard-screen/dashboard-screen.component.ts
+++ PWA/src/app/dashboard/dashboard-screen/dashboard-screen.component.ts
@@ -2,15 +2,28 @@ import { DashboardService } from './../../../shared/Services/db/dashboard.servic
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard-screen',
-  templateUrl: './dashboard-screen.component.html',
-  styleUrls: ['./dashboard-screen.component.scss']
+	selector: 'app-dashboard-screen',
+	templateUrl: './dashboard-screen.component.html',
+	styleUrls: ['./dashboard-screen.component.scss']
 })
 export class DashboardScreenComponent implements OnInit {
+	dashboardData: any = {
+		litres: 0,
+		prices: 0,
+		distance: 0,
+    balance: 0
+	};
 
-  constructor(private dashboardService: DashboardService) { }
+	constructor(private dashboardService: DashboardService) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	getDashboardData() {
+		this.dashboardService.getDashboardData().subscribe(
+			result => {
+        this.dashboardData = result;
+      },
+			err => {}
+		);
+	}
 }
