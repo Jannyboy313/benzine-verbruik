3e1b8ad94504221fdac9f54d36d5326f5ce191e8
Jan <s1120303@student.hsleiden.nl>
2021-07-14 16:40:37 +0200

    Added dashboard service to providers list

--- PWA/src/app/dashboard/dashboard.module.ts
+++ PWA/src/app/dashboard/dashboard.module.ts
@@ -5,15 +5,12 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardScreenComponent } from './dashboard-screen/dashboard-screen.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 
+import { DashboardService } from 'src/shared/Services/db/dashboard.service';
+
 
 @NgModule({
-  declarations: [
-    DashboardScreenComponent,
-    BarChartComponent
-  ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+	declarations: [DashboardScreenComponent, BarChartComponent],
+	imports: [CommonModule, DashboardRoutingModule],
+	providers: [DashboardService]
 })
-export class DashboardModule { }
+export class DashboardModule {}
