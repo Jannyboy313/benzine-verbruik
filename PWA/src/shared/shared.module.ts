e517dc80c738137e4f7a7edf79ce9f7d4f7f0668
Jan <s1120303@student.hsleiden.nl>
2021-07-28 12:49:11 +0200

    Generated scroll directive

--- PWA/src/shared/shared.module.ts
+++ PWA/src/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { ScrollDirective } from './directives/scroll.directive';
 
 @NgModule({
-	declarations: [ConfirmDialogComponent],
+	declarations: [ConfirmDialogComponent, ScrollDirective],
 	imports: [CommonModule, MatDialogModule],
 	exports: [ConfirmDialogComponent]
 })
