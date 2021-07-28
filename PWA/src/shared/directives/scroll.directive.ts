68a908851377cc2e17ebb886fa44219696a2a7aa
Jan <s1120303@student.hsleiden.nl>
2021-07-28 13:28:06 +0200

    Cleaned up scroll directive

--- PWA/src/shared/directives/scroll.directive.ts
+++ PWA/src/shared/directives/scroll.directive.ts
@@ -4,46 +4,18 @@ import { Directive, ElementRef } from '@angular/core';
 	selector: '[appScrollDirective]'
 })
 export class ScrollDirective {
-	previousScrollHeightMinusTop: number; // the variable which stores the distance
-	readyFor: string;
-	toReset = false;
+	scrollPosition: number;
 
 	constructor(public elementRef: ElementRef) {
-		this.previousScrollHeightMinusTop = 0;
-		this.readyFor = 'up';
-		this.restore();
-	}
-
-	reset() {
-		this.previousScrollHeightMinusTop = 0;
-		this.readyFor = 'up';
-		this.elementRef.nativeElement.scrollTop =
-			this.elementRef.nativeElement.scrollHeight;
-		// resetting the scroll position to bottom because that is where chats start.
+		this.scrollPosition = 0;
+		this.restore()
 	}
 
 	restore() {
-		if (this.toReset) {
-			if (this.readyFor === 'up') {
-				this.elementRef.nativeElement.scrollTop =
-					this.elementRef.nativeElement.scrollHeight -
-					this.previousScrollHeightMinusTop;
-				// restoring the scroll position to the one stored earlier
-			}
-			this.toReset = false;
-		}
+    this.elementRef.nativeElement.scrollTop = this.scrollPosition;
 	}
 
-	prepareFor(direction: any) {
-		this.toReset = true;
-		this.readyFor = direction || 'up';
-		this.elementRef.nativeElement.scrollTop = !this.elementRef.nativeElement
-			.scrollTop // check for scrollTop is zero or not
-			? this.elementRef.nativeElement.scrollTop + 1
-			: this.elementRef.nativeElement.scrollTop;
-		this.previousScrollHeightMinusTop =
-			this.elementRef.nativeElement.scrollHeight -
-			this.elementRef.nativeElement.scrollTop;
-		// the current position is stored before new messages are loaded
-	}
+	store() {
+    this.scrollPosition = this.elementRef.nativeElement.scrollTop;
+  }
 }
