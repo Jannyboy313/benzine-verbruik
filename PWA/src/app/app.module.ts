e340611f3768ea3de9347f716c1ebdf41f4f0dfa
Jan <s1120303@student.hsleiden.nl>
2021-08-11 12:42:41 +0200

    Formatted with prettier

--- PWA/src/app/app.module.ts
+++ PWA/src/app/app.module.ts
@@ -31,12 +31,12 @@ import { environment } from '../environments/environment';
 		SharedModule,
 		FuelModule,
 		DashboardModule,
-  ServiceWorkerModule.register('ngsw-worker.js', {
-    enabled: environment.production,
-    // Register the ServiceWorker as soon as the app is stable
-    // or after 30 seconds (whichever comes first).
-    registrationStrategy: 'registerWhenStable:30000'
-  })
+		ServiceWorkerModule.register('ngsw-worker.js', {
+			enabled: environment.production,
+			// Register the ServiceWorker as soon as the app is stable
+			// or after 30 seconds (whichever comes first).
+			registrationStrategy: 'registerWhenStable:30000'
+		})
 	],
 	bootstrap: [AppComponent]
 })
