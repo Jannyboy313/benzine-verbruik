460e8e4948df35a48149911b7fb8abd5b5418fe1
Jan van Overbeek <janvanoverbeek12@gmail.com>
2022-04-04 22:06:02 +0200

    Security/credentials fix (#47)
    * Added depends on by api for the database
    
    * Added restart on failure
    
    * Removed restart on failure
    
    * Updated mongoose
    
    * Removed deprecated settings
    
    * Added mongodb initialize script
    
    * Added default user on database initialize
    
    * Removed password value from access tokens
    
    * Updated documentation
    
    * Added enter
    
    * Added enter
    
    * Updated spelling
    
    * Put database_url in quotes
    
    * Removed bash styling from angular env

---
+++ docker/mongodb-init.js
@@ -0,0 +1,20 @@
+conn = new Mongo();
+db = conn.getDB('benzine');
+db.createUser({
+	user: 'test',
+	pwd: 'testpwd',
+	roles: [
+		{
+			role: 'readWrite',
+			db: 'benzine'
+		}
+	]
+});
+
+db.createCollection('users');
+db.users.insert({
+	email: 'test@test.nl',
+	password: '$2a$12$grFe9.QJGJKDJ2uvj9/8ee3kSg52JUopDNqmsqdJQfVxLU4.pyNri',
+    createdAt: ISODate("2022-04-04T19:47:23.737+00:00"),
+    updatedAt: ISODate("2022-04-04T19:47:23.737+00:00")
+});
