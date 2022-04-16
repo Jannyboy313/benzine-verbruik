9c9171be68b7627b127d6989cd1cc773695debd6
jannyboy313 <janvanoverbeek12@gmail.com>
2022-04-16 16:18:50 +0200

    Added input for filteredShown variable

--- web/src/shared/components/filter/filter.component.ts
+++ web/src/shared/components/filter/filter.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
-  selector: 'app-filter',
-  templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+	selector: 'app-filter',
+	templateUrl: './filter.component.html',
+	styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit, OnChanges {
+	private filterSettingsSubject: Subject<string> = new Subject<string>();
+  @Input() filterShown: boolean = false;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
+	ngOnInit(): void { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      this.filterShown = changes.filterShown.currentValue;
   }
-
 }
