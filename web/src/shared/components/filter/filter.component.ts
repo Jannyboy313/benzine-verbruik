48ec022da46bc51745adbe46ba91e46c75ff22e4
jannyboy313 <janvanoverbeek12@gmail.com>
2022-04-16 16:28:59 +0200

    Added filters model

--- web/src/shared/components/filter/filter.component.ts
+++ web/src/shared/components/filter/filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Filter } from 'src/shared/models/filter.model';
 
 @Component({
 	selector: 'app-filter',
@@ -9,6 +10,7 @@ import { Subject } from 'rxjs';
 export class FilterComponent implements OnInit, OnChanges {
 	private filterSettingsSubject: Subject<string> = new Subject<string>();
   @Input() filterShown: boolean = false;
+  @Input() filters: Filter[] = [];
 
 	constructor() {}
 
