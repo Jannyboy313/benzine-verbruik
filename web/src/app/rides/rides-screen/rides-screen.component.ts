be106f2451e436ba80146ba78065418721f030f6
jannyboy313 <janvanoverbeek12@gmail.com>
2022-04-16 16:28:26 +0200

    Added filters

--- web/src/app/rides/rides-screen/rides-screen.component.ts
+++ web/src/app/rides/rides-screen/rides-screen.component.ts
@@ -2,6 +2,7 @@ import { RidesListService } from './../../../shared/Services/rides-list-service'
 import { RideModalComponent } from './../ride-modal/ride-modal.component';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Filter } from 'src/shared/models/filter.model';
 
 @Component({
 	selector: 'app-rides-screen',
@@ -10,11 +11,21 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class RidesScreenComponent implements OnInit {
 	public filterShown: boolean = false;
+	public filters: Filter[] = []
 
 	constructor(
 		public dialog: MatDialog,
 		private ridesListService: RidesListService
-	) {}
+	) {
+		this.createFilters();
+	}
+
+	private createFilters(): void {
+		this.filters.push(new Filter('createdAt', 'Sorting on created'));
+		this.filters.push(new Filter('updatedAt', 'Sorting on last updated'));
+		this.filters.push(new Filter('distance', 'Sorting on distance'));
+		this.filters.push(new Filter('title', 'Sorting on title'));
+	}
 
 	ngOnInit(): void {}
 
