2f0377a24bc4f7f0214203d7397ecff8e9bafca3
jannyboy313 <janvanoverbeek12@gmail.com>
2022-04-17 11:32:34 +0200

    Added standard object id to user

--- docker/mongodb-init.js
+++ docker/mongodb-init.js
@@ -13,6 +13,7 @@ db.createUser({
 
 db.createCollection('users');
 db.users.insert({
+	_id: ObjectId('625bcdd583407077fd192c34'),
 	email: 'test@test.nl',
 	password: '$2a$12$grFe9.QJGJKDJ2uvj9/8ee3kSg52JUopDNqmsqdJQfVxLU4.pyNri',
 	createdAt: ISODate('2022-04-04T19:47:23.737+00:00'),
