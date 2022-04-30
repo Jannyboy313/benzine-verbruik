3fbd10993e1423d69abb1ad031b92d40d79d700a
jannyboy313 <janvanoverbeek12@gmail.com>
2022-04-30 12:43:26 +0200

    Fixed routing import

--- web/src/app/account/account.module.ts
+++ web/src/app/account/account.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountScreenComponent } from './account-screen/account-screen.component';
 
-
+import { AccountRoutingModule } from './account-routing.module';
 
 @NgModule({
   declarations: [
     AccountScreenComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AccountRoutingModule
   ]
 })
 export class AccountModule { }
