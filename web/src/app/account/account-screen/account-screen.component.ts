aede962f5f9a4ae74c76b6e0e06b64410b90a267
jannyboy313 <janvanoverbeek12@gmail.com>
2022-11-11 19:05:57 +0100

    Added logout functionality

--- web/src/app/account/account-screen/account-screen.component.ts
+++ web/src/app/account/account-screen/account-screen.component.ts
@@ -1,24 +1,45 @@
+import { Error } from './../../../shared/models/error.model';
+import { Router } from '@angular/router';
+import { LoginService } from './../../../shared/Services/login.service';
 import { Component, OnInit } from '@angular/core';
 import { DataStorageService } from 'src/shared/Services/data-storage.service';
 
 @Component({
-  selector: 'app-account-screen',
-  templateUrl: './account-screen.component.html',
-  styleUrls: ['./account-screen.component.scss']
+	selector: 'app-account-screen',
+	templateUrl: './account-screen.component.html',
+	styleUrls: ['./account-screen.component.scss']
 })
 export class AccountScreenComponent implements OnInit {
-  public email: string = '';
+	public email: string = '';
+	public error: Error = new Error();
 
-  constructor(private dataStorageService: DataStorageService) { }
+	constructor(
+		private dataStorageService: DataStorageService,
+		private loginService: LoginService,
+		private router: Router
+	) {}
 
-  ngOnInit(): void {
-    this.getEmail();
-  }
+	ngOnInit(): void {
+		this.getEmail();
+	}
 
-  private getEmail() {
-    let email = this.dataStorageService.getStoredData('user_email');
-    email = email === null ? 'No email found!' : email;
-    this.email = email;
-  }
+	private getEmail() {
+		let email = this.dataStorageService.getStoredData('user_email');
+		email = email === null ? 'No email found!' : email;
+		this.email = email;
+	}
 
+	public logout() {
+		this.loginService.logout().subscribe({
+			next: result => {
+        console.log("IM CALLED! RESULT", result);
+				this.dataStorageService.clearDataStorage();
+				this.router.navigate(['/login']);
+			},
+			error: err => {
+        console.log("IM CALLED! ERROR", err);
+        this.error.setError(true, "Logout has failed");
+      }
+		});
+	}
 }
